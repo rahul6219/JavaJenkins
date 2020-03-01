d3aa0c0029f0de43e5476d7f8588cd6b7aeb6ce3
Rahul Thakur <rahul6219@yahoo.com>
2020-03-01 13:36:16 -0800

    adding jenkins java program demo

---
+++ Hello.java
@@ -0,0 +1,15 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println("Hello World :::  "+i);
+		}
+	}
+
+}
